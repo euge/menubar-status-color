b56017877c1c282e5c08056bfc278c0e9701b545
Euge Gimelberg <egimelberg@invoca.com>
2014-08-17 15:38:32 -0700

    working notifications

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+  var shell = require('shelljs');
+
+  grunt.loadNpmTasks('grunt-download-atom-shell');
+
+  grunt.initConfig({
+    'download-atom-shell': {
+      version: '0.15.1',
+      outputDir: 'binaries'
+    }
+  });
+
+  grunt.registerTask("build", function() {
+    shell.rm('-rf', 'dist');
+    shell.mkdir('dist');
+    // copy vanilla Atom
+    shell.exec('cp -R binaries/Atom.app/ dist/MenubarStats.app');
+    // copy appliction source into our app
+    shell.exec('cp -R app dist/MenubarStats.app/Contents/Resources');
+
+  });
+
+};
---
+++ dist/MenubarStats.app/Contents/Resources/app/tray_display.js
@@ -0,0 +1,37 @@
+var Tray = require('tray');
+var moment = require('moment');
+var Menu = require('menu');
+var app = require('app');
+
+function imgForColor(color) {
+  return __dirname + "/imgs/" + color + ".png";
+}
+
+function TrayDisplay() {
+  this._tray = new Tray(imgForColor("grey"));
+  this._createMenu("No Project");
+}
+
+TrayDisplay.prototype = {
+
+  _createMenu : function(projectStatus) {
+    var menu = Menu.buildFromTemplate([
+      { label: projectStatus, type: 'normal', enabled : false },
+      { type: 'separator' },
+      { label: 'Quit', type: 'normal', click : function() { app.quit(); } }
+    ]);
+    this._tray.setContextMenu(menu);
+  },
+
+  setStatus : function(color, label) {
+    this._tray.setImage(imgForColor(color));
+    this._createMenu(label + " at " + this._nowTimestamp());
+  },
+
+  _nowTimestamp : function() {
+    return moment().format('M/D/YYYY, h:mm:ss A');
+  }
+
+}
+
+module.exports = TrayDisplay;
